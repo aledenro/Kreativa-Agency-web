c41f0f684cda4e0da900438cc3f29bf57c5fd59d
aledenro <133998613+aledenro@users.noreply.github.com>
2025-01-30 01:27:32 -0600

    crear estuctura front end

---
+++ client/kreativa-agency/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Navbar/Header";
+
+function App() {
+    return (
+        <>
+            <Header></Header>
+        </>
+    );
+}
+
+export default App;
