1856f476e8f7441343301fc52e31d3bf291dcc88
María José Valverde Peña <113710700+mjosevalverdep@users.noreply.github.com>
2025-01-31 18:41:38 -0600

    Agregando rutas de vistas y comentando rutas para que quede más claro a archivo App.jxs

--- client/kreativa-agency/src/App.jsx
+++ client/kreativa-agency/src/App.jsx
@@ -5,18 +5,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import AgregarCotizacion from "./pages/AgregarCotizacion";
 import CrearUsuario from "./pages/CrearUsuario";
+import Usuarios from "./pages/Usuarios";
+import VerUsuario from "./pages/VerUsuario";
+import EditarUsuario from "./pages/EditarUsuario";
 
 function App() {
     return (
         <Router>
             <Routes>
+                {/* Ruta para gestionar usuarios */}
+                <Route path="/usuarios" element={<Usuarios />} />
+
+                {/* Ruta para crear un nuevo usuario */}
+                <Route path="/usuario/crear" element={<CrearUsuario />} />
+
+                {/* Ruta para ver detalles de un usuario */}
+                <Route path="/usuario/:id" element={<VerUsuario />} />
+
+                {/* Ruta para editar un usuario */}
+                <Route path="/usuario/editar/:id" element={<EditarUsuario />} />
+
+                {/* Ruta existente para agregar cotización */}
                 <Route
                     path="/cotizacion/agregar"
                     element={<AgregarCotizacion />}
-                ></Route>
-                <Route
-                    path="/usuario/crear"
-                    element={<CrearUsuario />}
                 ></Route>
             </Routes>
         </Router>
