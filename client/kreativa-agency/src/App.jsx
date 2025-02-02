d1392f58abacf1c43a5d9b363ffed35a7c5fe880
valsv0602 <valeriasv0602@gmail.com>
2025-02-02 11:51:05 -0600

    modificar servicio frontend

--- client/kreativa-agency/src/App.jsx
+++ client/kreativa-agency/src/App.jsx
@@ -5,48 +5,49 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import AgregarCotizacion from "./pages/AgregarCotizacion";
 import VerCotizaciones from "./pages/verCotizaciones";
-import VerDetalleCotizacion from "./pages/VerDetalleCotizacion";
+// import VerDetalleCotizacion from "./pages/VerDetalleCotizacion";
 import CrearUsuario from "./pages/CrearUsuario";
 import Usuarios from "./pages/Usuarios";
 import VerUsuario from "./pages/VerUsuario";
 import EditarUsuario from "./pages/EditarUsuario";
 import AgregarServicio from "./pages/AgregarServicio";
+import ModificarServicio from "./pages/ModificarServicio";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                {/* Ruta para gestionar usuarios */}
-                <Route path="/usuarios" element={<Usuarios />} />
+  return (
+    <Router>
+      <Routes>
+        {/* Ruta para gestionar usuarios */}
+        <Route path="/usuarios" element={<Usuarios />} />
 
-                {/* Ruta para crear un nuevo usuario */}
-                <Route path="/usuario/crear" element={<CrearUsuario />} />
+        {/* Ruta para crear un nuevo usuario */}
+        <Route path="/usuario/crear" element={<CrearUsuario />} />
 
-                {/* Ruta para ver detalles de un usuario */}
-                <Route path="/usuario/:id" element={<VerUsuario />} />
+        {/* Ruta para ver detalles de un usuario */}
+        <Route path="/usuario/:id" element={<VerUsuario />} />
 
-                {/* Ruta para editar un usuario */}
-                <Route path="/usuario/editar/:id" element={<EditarUsuario />} />
+        {/* Ruta para editar un usuario */}
+        <Route path="/usuario/editar/:id" element={<EditarUsuario />} />
 
-                {/* Ruta existente para agregar cotización */}
-                <Route
-                    path="/cotizacion/agregar"
-                    element={<AgregarCotizacion />}
-                ></Route>
-                <Route
-                    path="/cotizacion/"
-                    element={<VerCotizaciones />}
-                ></Route>
-                <Route
-                    path="/cotizacion/:id"
-                    element={<VerDetalleCotizacion />}
-                ></Route>
+        {/* Ruta existente para agregar cotización */}
+        <Route
+          path="/cotizacion/agregar"
+          element={<AgregarCotizacion />}
+        ></Route>
+        <Route path="/cotizacion/" element={<VerCotizaciones />}></Route>
+        {/* <Route
+            path="/cotizacion/:id"
+            element={<VerDetalleCotizacion />}
+          ></Route> */}
 
         <Route path="/servicio/agregar" element={<AgregarServicio />}></Route>
-            </Routes>
-        </Router>
-    );
-
+        <Route
+          path="/servicio/modificar/:id"
+          element={<ModificarServicio />}
+        ></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
