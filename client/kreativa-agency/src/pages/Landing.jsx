0bdabdc5afe827a5113e4c0430e207a82edae233
valsv0602 <valeriasv0602@gmail.com>
2025-03-24 15:39:59 -0600

    apariencia landing

--- client/kreativa-agency/src/pages/Landing.jsx
+++ client/kreativa-agency/src/pages/Landing.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import video from "../assets/vid/landing-video.mov";
+import React, { useEffect } from "react";
+// import video from "../assets/vid/landing-video.mov";
 import Navbar from "../components/Navbar/Navbar";
 import ListadoServicios from "./ListadoServicios";
 import FormContacto from "./FormContacto";
 import FormReclutaciones from "./FormReclutaciones";
-import "animate.css";
+import Particles from "../components/ui/Particles";
 
 const Landing = () => {
     return (
@@ -14,7 +14,7 @@ const Landing = () => {
                 <div className="landing-container">
                     <div className="video-section">
                         <video className="video-background" autoPlay loop muted>
-                            <source src={video} type="video/mp4" />
+                            {/* <source src={video} type="video/mp4" /> */}
                         </video>
                         <div className="video-overlay"></div>
                         <div className="video-content">
@@ -35,6 +35,26 @@ const Landing = () => {
             </section>
             <section id="contacto">
                 <FormContacto></FormContacto>
+            </section>
+            <section className="particles">
+                <div
+                    style={{
+                        width: "100%",
+                        height: "600px",
+                        position: "relative",
+                    }}
+                >
+                    <Particles
+                        particleColors={["#ffffff", "#ffffff"]}
+                        particleCount={200}
+                        particleSpread={10}
+                        speed={0.1}
+                        particleBaseSize={100}
+                        moveParticlesOnHover={true}
+                        alphaParticles={false}
+                        disableRotation={false}
+                    />
+                </div>
             </section>
             <section id="reclutaciones">
                 <FormReclutaciones></FormReclutaciones>
