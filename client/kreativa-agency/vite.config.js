7270f40da5298b9ba859c4f3318b86fcb4ef6a76
María José Valverde Peña <113710700+mjosevalverdep@users.noreply.github.com>
2025-03-30 22:09:12 -0600

    Terminando 100% Vista Log-In Listo tanto diseño como responsive y funcionalidad

--- client/kreativa-agency/vite.config.js
+++ client/kreativa-agency/vite.config.js
@@ -1,13 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      '60db-201-207-235-3.ngrok-free.app' // Agrega aquí tu host de ngrok
-    ]
-  }
 })
-
