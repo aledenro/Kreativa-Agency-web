a8b2c12d2984e386355ea920775b77475a8469ea
María José Valverde Peña <113710700+mjosevalverdep@users.noreply.github.com>
2025-04-02 15:08:05 -0600

    Envolviendo  App con el Provider

--- client/kreativa-agency/src/main.jsx
+++ client/kreativa-agency/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HeroUIProvider } from "@heroui/system"; // ✅ Importa HeroUIProvider
+import { HeroUIProvider } from "@heroui/system"; // ✅
+import { AuthProvider } from "./context/AuthContext"; // ✅ Importa el AuthProvider
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <HeroUIProvider>
-            <App />
+            <AuthProvider> 
+                <App />
+            </AuthProvider>
         </HeroUIProvider>
     </StrictMode>
 );
