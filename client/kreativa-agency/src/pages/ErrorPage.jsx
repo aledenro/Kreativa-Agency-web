6b7c83789436ae1ff1331c6e147c1e50e972c38f
aledenro <133998613+aledenro@users.noreply.github.com>
2025-08-05 00:04:35 -0600

    estilos pagina de error

--- client/kreativa-agency/src/pages/ErrorPage.jsx
+++ client/kreativa-agency/src/pages/ErrorPage.jsx
@@ -6,14 +6,19 @@ const ErrorPage = () => {
     const { errorCode = "Error", mensaje = "Ha ocurrido algo" } = state || {};
 
     return (
-        <div className="error-container">
-            <div className="error-logo">
-                <img src="src/assets/img/logo.png" alt="Logo" />
+        <div className="error-container d-flex flex-column justify-content-center align-items-center vh-100">
+            <div className="error-logo mb-4">
+                <img
+                    src="src/assets/img/logo.png"
+                    alt="Logo"
+                    className="img-fluid"
+                />
             </div>
-            <div className="error-content">
-                <h1>{errorCode}</h1>
-                <p>{mensaje}</p>
-                <a href="/" className="btn thm-btn">
+
+            <div className="error-content text-center">
+                <h1 className="display-1 mb-3">{errorCode}</h1>
+                <p className="fs-4 mb-4">{mensaje}</p>
+                <a href="/" className="btn thm-btn btn-lg">
                     Ir a la Página Principal
                 </a>
             </div>
@@ -22,9 +27,8 @@ const ErrorPage = () => {
 };
 
 ErrorPage.propTypes = {
-    errorCode: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-        .isRequired,
-    mensaje: PropTypes.string.isRequired,
+    errorCode: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    mensaje: PropTypes.string,
 };
 
 export default ErrorPage;
